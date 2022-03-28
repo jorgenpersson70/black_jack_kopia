2e32b2d1525527ffae6f518bd44d5f569795d3ef
jörgen persson <jorgen_raby@icloud.com>
2022-03-28 09:13:29 +0200

    hej

---
+++ app/src/main/java/com/example/image_test/PlayVoice.kt
@@ -0,0 +1,31 @@
+package com.example.image_test
+
+import android.media.MediaPlayer
+/*
+fun playSound(start : Boolean) {
+
+    var resId =getResources().getIdentifier(
+        R.raw.tal.toString(),
+        "raw", this?.packageName
+    )
+
+
+    if (start) {
+        if(mediaPlayer == null)
+        {
+            mediaPlayer = MediaPlayer.create(this, resId)
+            mediaPlayer!!.start()
+        } else {
+            mediaPlayer!!.seekTo(0)
+            mediaPlayer!!.start()
+        }
+    } else {
+
+        if(mediaPlayer != null)
+        {
+            mediaPlayer!!.pause()
+        }
+
+
+    }
+}*/
